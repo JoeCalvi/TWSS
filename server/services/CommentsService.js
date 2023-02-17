5e0eb99366240f2045c21367a64e3f9242b7e68d
Ryan Wittman <OfficiallyRy@outlook.com>
2023-02-17 14:29:23 -0700

    Didn't have my returns lol

--- server/services/CommentsService.js
+++ server/services/CommentsService.js
@@ -4,9 +4,11 @@ import { dbContext } from "../db/DbContext.js"
 class CommentsService{
   async createComment(commentData) {
     const comment = await dbContext.Comments.create(commentData)
+    return comment
   }
   async getPostComments(postId) {
     const comments = await dbContext.Comments.find({postId})
+    return comments
   }
 
 }
