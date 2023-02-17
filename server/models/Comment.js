ec548a59d9e0d0abc3de498c66b5f10e088de0cd
Ryan Wittman <OfficiallyRy@outlook.com>
2023-02-17 14:09:30 -0700

    CommentSchema done hopefully

---
+++ server/models/Comment.js
@@ -0,0 +1,19 @@
+import { Schema } from "mongoose"
+
+const ObjectId = Schema.Types.ObjectId
+
+export const CommentSchema = new Schema(
+  {
+    commenterId: {type: ObjectId, ref: 'Account', required: true},
+    description: {type: String, required: true, maxLength: 500},
+    postId: {type: ObjectId, ref: 'Post', required: true}
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+)
+
+CommentSchema.virtual('post', {
+  localField: 'postId',
+  foreignField: '_id',
+  justOne: true,
+  ref: 'Post'
+})
