05faa1e94f395bbf714ba96a14ace14edec757a9
Parker <ward.parker26@gmail.com>
2023-02-17 15:14:11 -0700

    create posts

--- server/models/Post.js
+++ server/models/Post.js
@@ -5,7 +5,8 @@ export const PostSchema = new Schema(
   {
     title: { type: String, required: true },
     description: { type: String, required: true, maxLength: 500 },
-    posterId: { type: ObjectId, ref: 'Account', required: true }
+    posterId: { type: ObjectId, ref: 'Account', required: true },
+    tag: { type: String, enum: ["family", "house work", "friends", "job"], required: true }
 
   }, { timestamps: true, toJSON: { virtuals: true } }
 )
--- server/services/PostsService.js
+++ server/services/PostsService.js
@@ -1,6 +1,10 @@
 import { dbContext } from "../db/DbContext.js"
 
 class PostsService {
+  async createPosts(body) {
+    const newPost = await dbContext.Posts.create(body)
+    return newPost
+  }
   async getPosts() {
     const posts = await dbContext.Posts.find()
     return posts
