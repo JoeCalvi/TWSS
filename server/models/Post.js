18ccd274a8160d717d5a3b20621709097b503ab2
Parker <ward.parker26@gmail.com>
2023-02-17 16:57:22 -0700

    housework is one word you dummy

--- server/models/Post.js
+++ server/models/Post.js
@@ -6,7 +6,7 @@ export const PostSchema = new Schema(
     title: { type: String, required: true },
     description: { type: String, required: true, maxLength: 500 },
     posterId: { type: ObjectId, ref: 'Account', required: true },
-    tag: { type: String, enum: ["family", "house work", "friends", "job"], required: true }
+    tag: { type: String, enum: ["family", "housework", "friends", "job"], required: true }
 
   }, { timestamps: true, toJSON: { virtuals: true } }
 );
