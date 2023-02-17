fded379ffea9f55047f9ac982bbc8123e7104e7b
Parker <ward.parker26@gmail.com>
2023-02-17 15:32:59 -0700

    get Posts by Id

--- server/services/PostsService.js
+++ server/services/PostsService.js
@@ -1,6 +1,16 @@
 import { dbContext } from "../db/DbContext.js"
+import { BadRequest } from "../utils/Errors.js"
 
 class PostsService {
+  async getPostById(postId) {
+    const post = await dbContext.Posts.findById(postId)
+    // .populate('Poster', 'name picture')
+    // .populate('')
+    if (!post) {
+      throw new BadRequest("Post Not Found")
+    }
+    return post
+  }
   async createPosts(body) {
     const newPost = await dbContext.Posts.create(body)
     return newPost
