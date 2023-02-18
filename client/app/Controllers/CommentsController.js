dc5b653f53e735e165a7d4b8642b31900c5bdeec
Scotty Youngblood <scottybyoungblood@gmail.com>
2023-02-17 19:47:11 -0700

    get comments

--- client/app/Controllers/CommentsController.js
+++ client/app/Controllers/CommentsController.js
@@ -1,8 +1,19 @@
+import { commentsService } from "../Services/CommentsService.js"
+import { Pop } from "../Utils/Pop.js"
 
 
 
 export class CommentsController{
   constructor(){
-    console.log("CommentsController says hello");
+    
+  }
+
+  async getAllComments() {
+    try {
+      await commentsService.getAllComments()
+    } catch (error) {
+      console.error(error)
+      Pop.error(error.message)
+    }
   }
 }
