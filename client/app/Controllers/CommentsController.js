c45701f31e127fbbd3ed565a7d51cb6dd96af9e3
Scotty Youngblood <scottybyoungblood@gmail.com>
2023-02-17 21:25:59 -0700

    create comment

--- client/app/Controllers/CommentsController.js
+++ client/app/Controllers/CommentsController.js
@@ -2,6 +2,7 @@ import { appState } from "../AppState.js"
 import { commentsService } from "../Services/CommentsService.js"
 import { Pop } from "../Utils/Pop.js"
 import { setHTML } from "../Utils/Writer.js"
+import { getFormData } from "../Utils/FormHandler.js"
 
 function _drawCommentsByPostId(){
   let template = ''
@@ -18,6 +19,20 @@ export class CommentsController{
     try {
       await commentsService.getCommentsByPostId(postId)
       await _drawCommentsByPostId()
+    } catch (error) {
+      console.error(error)
+      Pop.error(error.message)
+    }
+  }
+
+  async createComment(){
+    try {
+      // @ts-ignore
+      window.event.preventDefault()
+      // @ts-ignore
+      let form = window.event.target
+      let commentBody = getFormData(form)
+      await commentsService.createComment(commentBody)
     } catch (error) {
       console.error(error)
       Pop.error(error.message)
--- client/app/Services/CommentsService.js
+++ client/app/Services/CommentsService.js
@@ -4,6 +4,10 @@ import { appState } from "../AppState.js";
 import { Comment } from "../Models/Comment.js";
 import { server } from "./AxiosService.js";
 class CommentsService{
+  async createComment(commentBody) {
+    const res = await server.post('api/comments', commentBody)
+    console.log(res.data);
+  }
   async getCommentsByPostId(postId) {
     const res = await server.get(`api/posts/${postId}/comments`);
     appState.comments = res.data.map(c => new Comment(c));
