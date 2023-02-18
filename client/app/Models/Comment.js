4d0f68b6b047dd48032c6b0811560cc8652e57cd
Ry <OfficiallyRy@outlook.com>
2023-02-18 16:54:24 -0700

    css is superior I hate scss

--- client/app/Models/Comment.js
+++ client/app/Models/Comment.js
@@ -14,7 +14,7 @@ export class Comment {
     return /*html*/ `
             <div class="col-2 ms-3 my-3">
               <img src="${this.commenter?.picture}" alt="" class="rounded-circle all-posts-img">
-              <span class="fs-5 ps-3">${this.commenter?.name}</span>
+              <span class="fs-5 ps-3 normal-text">${this.commenter?.name}</span>
             </div>
             <div class="col-8 d-flex align-items-center glass-card rounded p-4">
               <div class="text-start">
